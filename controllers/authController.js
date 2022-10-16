7a8083d168cf713bd3f3377293389233ee3734e5
Ahmet İhsan Bozacı <ahmetbozac@gmail.com>
2022-10-16 21:45:05 +0300

    special dashboards

--- controllers/authController.js
+++ controllers/authController.js
@@ -27,7 +27,7 @@ exports.loginUser = async (req, res) => {
           if (login) {
             //User Session
             req.session.userID = user.email;
-            res.status(200).redirect("/");
+            res.status(200).redirect("/users/dashboard");
           }
         });
       }
@@ -45,3 +45,12 @@ exports.logoutUser = (req, res) => {
     res.redirect("/");
   });
 };
+
+//Dashboard
+exports.getDashboardPage =  async (req, res) => {
+  const user = await User.findOne({email:req.session.userID})
+  res.status(200).render('dashboard', {
+    page_name: 'dashboard',
+    user
+  });
+}; 
