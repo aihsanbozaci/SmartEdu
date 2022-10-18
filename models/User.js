7fa54e450a7b6bc482f1f00b668ca5ccf23eb2bc
Ahmet İhsan Bozacı <ahmetbozac@gmail.com>
2022-10-18 16:17:55 +0300

    user roles

--- models/User.js
+++ models/User.js
@@ -2,10 +2,10 @@ const mongoose = require("mongoose");
 const bcrypt = require("bcrypt");
 const Schema = mongoose.Schema;
 
-mongoose.set('useNewUrlParser', true);
-mongoose.set('useFindAndModify', false);
-mongoose.set('useCreateIndex', true);
-mongoose.set('useUnifiedTopology', true);
+mongoose.set("useNewUrlParser", true);
+mongoose.set("useFindAndModify", false);
+mongoose.set("useCreateIndex", true);
+mongoose.set("useUnifiedTopology", true);
 
 const UserSchema = new Schema({
   name: {
@@ -21,14 +21,22 @@ const UserSchema = new Schema({
     type: String,
     required: true,
   },
+  role: {
+    type: String,
+    enum: ["Student", "Teacher", "admin","Admin"],
+    default: "Student",
+  },
 });
 //password crypt
-UserSchema.pre('save',function (next){
+UserSchema.pre("save", function (next) {
   const user = this;
-  bcrypt.hash(user.password,10, (error, hash)=>{
+  bcrypt.hash(user.password, 10, (error, hash) => {
     user.password = hash;
     next();
-  })
-})
+  });
+  if(user.role==='Admin','admin')(res)=>{
+    return res.status(401).send('You are not allowed to access!');
+  };
+});
 const User = mongoose.model("User", UserSchema);
 module.exports = User;
--- routes/courseRoute.js
+++ routes/courseRoute.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const courseController = require('../controllers/courseController');
 const router = express.Router();
+const roleMiddleware = require('../middlewares/roleMiddleware');
+
 
 router.route('/').post(courseController.createCourse); //localhost:3000/courses will work with it
 router.route('/').get(courseController.getAllCourses);
 router.route('/:slug').get(courseController.getCourse);
+router.route('/').post(roleMiddleware(["teacher","admin"]), courseController.createCourse); //admin and teachers can create courses
 
 module.exports = router;
